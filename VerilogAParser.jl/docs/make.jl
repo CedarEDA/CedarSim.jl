fd50d136aa52317234cbf8e2b74c3ec20be0fb3c
Fredrik Ekre <ekrefredrik@gmail.com>
2024-04-30 10:57:41 +0200

    Initial buildkite configuration

--- VerilogAParser.jl/docs/make.jl
+++ VerilogAParser.jl/docs/make.jl
@@ -1,22 +1,6 @@
 using Documenter
 using VerilogAParser
 
-# Normalize the documenter key if it's not already base64-encoded
-using Base64
-documenter_key = get(ENV, "DOCUMENTER_KEY", "")
-try
-    base64decode(documenter_key)
-catch e
-    if isa(e, ArgumentError)
-        if !endswith(documenter_key, "\n")
-            global documenter_key = string(documenter_key, "\n")
-        end
-        ENV["DOCUMENTER_KEY"] = base64encode(documenter_key)
-    else
-        rethrow(e)
-    end
-end
-
 makedocs(
     sitename = "VerilogAParser",
     format = Documenter.HTML(),
@@ -25,6 +9,6 @@ makedocs(
 )
 
 deploydocs(
-    repo = "github.com/JuliaComputing/CedarSim.jl.git",
-    branch = "docs-verilogaparser",
+    repo = "github.com/CedarEDA/CedarSim.jl.git",
+    branch = "gh-pages-verilogaparser",
 )
--- docs/make.jl
+++ docs/make.jl
@@ -15,24 +15,6 @@ makedocs(
     warnonly = true,
 )
 
-
-# Normalize the documenter key if it's not already base64-encoded
-using Base64
-documenter_key = get(ENV, "DOCUMENTER_KEY", "")
-try
-    base64decode(documenter_key)
-catch e
-    if isa(e, ArgumentError)
-        if !endswith(documenter_key, "\n")
-            global documenter_key = string(documenter_key, "\n")
-        end
-        ENV["DOCUMENTER_KEY"] = base64encode(documenter_key)
-    else
-        rethrow(e)
-    end
-end
-
 deploydocs(
-    repo = "github.com/JuliaComputing/CedarSim.jl.git",
-    branch = "docs",
+    repo = "github.com/CedarEDA/CedarSim.jl.git",
 )
